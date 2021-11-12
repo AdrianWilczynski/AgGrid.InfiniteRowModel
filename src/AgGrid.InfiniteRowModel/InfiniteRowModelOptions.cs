70cd38e02fbc68f17255874006fb976a30951d96
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2021-11-12 09:50:15 +0100

    Cleanup

--- src/AgGrid.InfiniteRowModel/InfiniteRowModelOptions.cs
+++ src/AgGrid.InfiniteRowModel/InfiniteRowModelOptions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AgGrid.InfiniteRowModel
+﻿namespace AgGrid.InfiniteRowModel
 {
     public class InfiniteRowModelOptions
     {
